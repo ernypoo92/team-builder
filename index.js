eb5e2e0536eb35ca32f668a3a1a1473d43a9d808
Aaron Jones <ernypoo92@gmail.com>
2022-03-19 12:07:05 -0600

    set up repo

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquire = require ("inquirer");
+const table = require ("console.table");
+const { Console } = require("console");
+
+const newRoster = () => {
+    console.log("Welcome to Team Builder!");
+    console.log("Who will be your Team Lead/Manager?");
+    
+}
