770cf176b8bb229b57ba1636c6e4a21e82b2e998
Aaron Jones <ernypoo92@gmail.com>
2022-04-06 19:03:41 -0600

    fixed issues with fs

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Manager = require('./lib/Manager')
+const path = require('path');
+const HTMLpage= require ('./src/HTMLpage')
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+
+//Global Arrays
+const employees = [];
+const managerArr = [];
+const engineersArr = [];
+const internsArr = [];
+
 
 promptManager = () => {
     // user instruction
@@ -54,10 +66,10 @@ promptManager = () => {
         },
         {
             type: 'text',
-            name: 'officeNumber',
+            name: 'officeNum',
             message: "What's the team manager's office number? (Required)",
-            validate: officeInput => {
-                if (officeInput) {
+            validate: officeNumInput => {
+                if (officeNumInput) {
                     return true;
                 } else {
                     console.log("Please enter the team manager's office number!");
@@ -76,6 +88,153 @@ promptManager = () => {
         menu();
     });
 };
+promptEngineer = () => {
+    // user instruction
+    console.log(`
+    -----------------------------------------------
+        Enter your New Engineer's information.
+    -----------------------------------------------
+    `);
+
+    // user prompts for manager's name, employee ID, email, and office number
+    inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: "What is the Engineer's name? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Engineer's name!");
+                    return false;
+                };
+            }
+        },
+        {
+            type: 'text',
+            name: 'employeeId',
+            message: "What's the Engineer's employee ID? (Required)",
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Engineer's employee ID!");
+                    return false;
+                };
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "What's the Engineer's email? (Required)",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Engineer's email!");
+                    return false;
+                };
+            }
+        },
+        {
+            type: 'text',
+            name: 'github',
+            message: "What's the Engineer's Github Username? (Required)",
+            validate: officeNumInput => {
+                if (officeNumInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Engineer's Github Username!");
+                    return false;
+                }
+            }
+        }
+    ]).then((engineer) => {
+        // send prompt info to Manager object
+        const engineerInfo = new Engineer(engineer.name, engineer.employeeId, engineer.email, engineer.github);
+
+        // push to managerArr
+        engineersArr.push(engineerInfo);
+
+        // prompt new team member
+        menu();
+    });
+};
+
+promptIntern = () => {
+    // user instruction
+    console.log(`
+    -----------------------------------------------
+        Enter your New Intern's information.
+    -----------------------------------------------
+    `);
+
+    // user prompts for manager's name, employee ID, email, and office number
+    inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: "What is the Intern's name? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Intern's name!");
+                    return false;
+                };
+            }
+        },
+        {
+            type: 'text',
+            name: 'employeeId',
+            message: "What's the Intern's employee ID? (Required)",
+            validate: employeeIdInput => {
+                if (employeeIdInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Intern's employee ID!");
+                    return false;
+                };
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "What's the Intern's email? (Required)",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Intern's email!");
+                    return false;
+                };
+            }
+        },
+        {
+            type: 'text',
+            name: 'school',
+            message: "What's the Intern's School? (Required)",
+            validate: officeNumInput => {
+                if (officeNumInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Intern's School!");
+                    return false;
+                }
+            }
+        }
+    ]).then((intern) => {
+        // send prompt info to Manager object
+        const internInfo = new Intern(intern.name, intern.employeeId, intern.email, intern.github);
+
+        // push to managerArr
+        internsArr.push(internInfo);
+
+        // prompt new team member
+        menu();
+    });
+};
 
 // get user input for adding an additional team member
 menu = function() {
@@ -83,24 +242,31 @@ menu = function() {
     inquirer.prompt([
         {
             type: 'list',
-            name: 'eOrI',
+            name: 'mainMenu',
             message: "Choose one of the following options",
-            choices: ['Add New Engineer', 'Add New Intern', 'Finnish and Publish Team']            
+            choices: ['Add New Engineer', 'Add New Intern', 'Finish and Publish Team']            
         }
-    ]).then(({ eOrI}) => {
-            if(eOrI === 'New Engineer') {
+    ]).then(({mainMenu}) => {
+            if(mainMenu === 'Add New Engineer') {
                 // if engineer is selected, run prompt engineer prompts
                 promptEngineer();
-            } else if(eOrI === 'New Intern') {
+            } else if(mainMenu === 'Add New Intern') {
                 // if intern is selected, run prompt intern prompts
                 promptIntern();
-            } else if(eOrI === 'Complete') {
+            } else if(mainMenu === 'Finish and Publish Team') {
                 employees.push(managerArr);
                 employees.push(engineersArr);
                 employees.push(internsArr);
 
-                generateRoster();
+                generateTeam();
             }
         });
 };
+
+// generate HTML page
+generateTeam = () => {
+    fs.writeFileSync(path.join(__dirname, 'dist/index.html'), HTMLpage(employees), 'utf-8');
+};
+
+//start with promptManager function
 promptManager();
